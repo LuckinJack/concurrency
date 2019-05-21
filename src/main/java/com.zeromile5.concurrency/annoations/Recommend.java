2d8f4fe335dab75fb3e3d5eb4717314a0ae9e46f
ZeroMile5 <1765209006@qq.com>
2019-05-21 21:33:24 +0800

    add: 辅助标记用注解类

---
+++ src/main/java/com.zeromile5.concurrency/annoations/Recommend.java
@@ -0,0 +1,18 @@
+package com.zeromile5.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用于标记【推荐】的类或者写法
+ * Created by ZhangMiao
+ * Date : 2019/4/16
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+}
